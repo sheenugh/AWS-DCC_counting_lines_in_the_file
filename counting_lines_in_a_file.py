8b8fd4eb3b7bdd768cbf0dab29c406b8a8084007
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-02-20 01:28:31 +0800

    done

--- counting_lines_in_a_file.py
+++ counting_lines_in_a_file.py
@@ -1,4 +1,16 @@
 # ========== PSEUDO CODE ==========
 # - Def function
+def counting_lines_in_a_text():
+    file = open("story.txt","r")
+    count= 0
+    
 # - Using for loop
+    for i in file:
+        count+= 1
+    file.close()
+    
+# - Printing the conclusion
+    print("The number of lines in the 'story.txt' file is", count) 
+    
 # - Caller of the def function
+counting_lines_in_a_text()
